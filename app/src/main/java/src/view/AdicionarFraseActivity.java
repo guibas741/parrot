b49ca875e9dec66496233543fae11823ce69a447
Guilherme <Guilherme>
2017-09-04 23:12:42 -0300

    Adiciona campos e botão para a tela de criação de frases

--- app/src/main/java/src/view/AdicionarFraseActivity.java
+++ app/src/main/java/src/view/AdicionarFraseActivity.java
@@ -1,15 +1,28 @@
 package src.view;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.android.parrot.parrot.R;
 
 public class AdicionarFraseActivity extends AppCompatActivity {
 
+    private Button btnAddFrase;
+    private EditText txtFraseOriginal, txtFraseTraduzida, txtCategoria, txtFavorito;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adicionar_frase);
+
+        btnAddFrase = (Button) findViewById(R.id.btnAddFrasesId);
+
+        txtFraseOriginal = (EditText) findViewById(R.id.txtFraseOriginalId);
+        txtFraseTraduzida = (EditText) findViewById(R.id.txtFraseTraduzidaId);
+        txtCategoria = (EditText) findViewById(R.id.txtCategoriaId);
+        txtFavorito = (EditText) findViewById(R.id.txtFavId);
     }
+
+
 }
