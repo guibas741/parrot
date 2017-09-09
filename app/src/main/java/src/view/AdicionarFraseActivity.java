c771e7513f7951eb36d174e485f681e25d4e6055
Guilherme <Guilherme>
2017-09-09 17:35:47 -0300

    Substitui editText por checkBox do atributo favorito

--- app/src/main/java/src/view/AdicionarFraseActivity.java
+++ app/src/main/java/src/view/AdicionarFraseActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -18,7 +19,8 @@ import src.model.Frase;
 public class AdicionarFraseActivity extends AppCompatActivity {
 
     private Button btnAddFrase, btnListar;
-    private EditText txtFraseOriginal, txtFraseTraduzida, txtCategoria, txtFavorito;
+    private EditText txtFraseOriginal, txtFraseTraduzida, txtCategoria;
+    private CheckBox favorito;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,7 +32,8 @@ public class AdicionarFraseActivity extends AppCompatActivity {
         txtFraseOriginal = (EditText) findViewById(R.id.txtFraseOriginalId);
         txtFraseTraduzida = (EditText) findViewById(R.id.txtFraseTraduzidaId);
         txtCategoria = (EditText) findViewById(R.id.txtCategoriaId);
-        txtFavorito = (EditText) findViewById(R.id.txtFavId);
+
+        favorito = (CheckBox) findViewById(R.id.favoritoId);
 
         Create c = new Create(getApplicationContext());
         c.createTable();
@@ -43,7 +46,7 @@ public class AdicionarFraseActivity extends AppCompatActivity {
                 f.setFraseOriginal(txtFraseOriginal.getText().toString());
                 f.setFraseTraduzida(txtFraseTraduzida.getText().toString());
                 f.setCategoria(txtCategoria.getText().toString());
-                f.setFavorito(Boolean.parseBoolean(txtFavorito.getText().toString()));
+                f.setFavorito(favorito.isChecked());
 
                 Create c = new Create(getApplicationContext());
 
